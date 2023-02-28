2a59605c8cd412b095f7067f00de770af4bba726
VINICIUS DO CARMO SILVA <0050506@IFMG.EDU.BR>
2023-02-28 16:23:11 -0300

    qualquer

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Ceguin!</Text>
+
+      <View style={styles.row}>
+
+      <TouchableOpacity style={styles.button}>
+        <text style={styles}>Entrar</text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button, { backgroundColor: '#9b111e'}]}>
+        <text style={styles}>Voltar</text>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#434250',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    color: '#fff',
+  },
+  button: {
+    marginHorizontal: 10,
+    marginVertical: 20,
+    backgroundColor: '#0A0',
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 20,
+  },
+  textButton: {
+    color: "fff",
+    fontSize: 20,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  row: {
+    flexDirection: 'row',
   },
 });
